badcb662e47d9c04944bc902eb8e8239f4fd245e
Jervis <38509081+jervisc@users.noreply.github.com>
2019-03-31 13:32:09 -0400

    Update Edge.java
    Edge is the status for each Node

--- Edge.java
+++ Edge.java
@@ -9,124 +9,22 @@ package bmo.test;
  *
  * @author CHENJE8
  */
-import java.util.ArrayList;
-import java.util.Hashtable;
+public class Edge {
 
-public class Graph {
-    public Hashtable<Node, Edge> routeTable;
+    public Node from;
+    public Node to;
+    public int weight;
+    public Edge next;
 
-    public Graph() {
-        this.routeTable = new Hashtable<Node, Edge>();
+    public Edge(Node from, Node to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.next = null;
     }
 
-    public int getDistance(ArrayList<Node> cities) throws Exception {
-        if (cities.size() < 2) {
-            return 0;
-        }
-        int distance = 0;
-        int depth = 0;
-        int i = 0;
-        while (i < cities.size() - 1) {
-            if (this.routeTable.containsKey(cities.get(i))) {
-                Edge route = this.routeTable.get(cities.get(i));
-                while (route != null) {
-                    if (route.to.equals(cities.get(i + 1))) {
-                        distance += route.weight;
-                        depth++;
-                        break;
-                    }
-                    route = route.next;
-                }
-            } else {
-                throw new Exception("NO SUCH ROUTE");
-            }
-            i++;
-        }
-        if (depth != cities.size() - 1) {
-            throw new Exception("NO SUCH ROUTE");
-        }
-        return distance;
-    }
-
-    public int calculateNumbsOfStops(Node start, Node end, int depth, int maxStops) throws Exception {
-        int routes = 0;
-        if (this.routeTable.containsKey(start) && this.routeTable.containsKey(end)) {
-            depth++;
-            if (depth > maxStops) {
-                return 0;
-            }
-            start.visited = true;
-            Edge edge = this.routeTable.get(start);
-            while (edge != null) {
-                if (edge.to.equals(end)) {
-                    routes++;
-                    edge = edge.next;
-                    continue;
-                } else if (!edge.to.visited) {
-                    routes += calculateNumbsOfStops(edge.to, end, depth, maxStops);
-                    depth--;
-                }
-                edge = edge.next;
-            }
-        } else {
-            throw new Exception("NO SUCH ROUTE");
-        }
-        start.visited = false;
-        return routes;
-    }
-
-    public int calculateShortestPath(Node start, Node end, int weight, int shortestRoute) throws Exception {
-        if (this.routeTable.containsKey(start) && this.routeTable.containsKey(end)) {
-            start.visited = true;
-            Edge edge = this.routeTable.get(start);
-            while (edge != null) {
-                if (edge.to == end || !edge.to.visited) {
-                    weight += edge.weight;
-                }
-                if (edge.to.equals(end)) {
-                    if (shortestRoute == 0 || weight < shortestRoute) {
-                        shortestRoute = weight;
-                    }
-                    start.visited = false;
-                    return shortestRoute;
-                } else if (!edge.to.visited) {
-                    shortestRoute = calculateShortestPath(edge.to, end, weight, shortestRoute);
-                    weight -= edge.weight;
-                }
-                edge = edge.next;
-            }
-        } else {
-            throw new Exception("NO SUCH ROUTE");
-        }
-        start.visited = false;
-        return shortestRoute;
-    }
-
-    public int getNumsOfRoutes(Node start, Node end, int weight, int maxDistance) throws Exception {
-        // for Maximum distance
-        int routes = 0;
-        if (this.routeTable.containsKey(start) && this.routeTable.containsKey(end)) {
-            Edge edge = this.routeTable.get(start);
-            while (edge != null) {
-                weight += edge.weight;
-                if (weight <= maxDistance) {
-                    if (edge.to.equals(end)) {
-                        routes++;
-                        routes += getNumsOfRoutes(edge.to, end, weight, maxDistance);
-                        edge = edge.next;
-                        continue;
-                    } else {
-                        routes += getNumsOfRoutes(edge.to, end, weight, maxDistance);
-                        weight -= edge.weight;
-                    }
-                } else {
-                    weight -= edge.weight;
-                }
-                edge = edge.next;
-            }
-        } else {
-            throw new Exception("NO SUCH ROUTE");
-        }
-        return routes;
+    public Edge next(Edge edge) {
+        this.next = edge;
+        return this;
     }
 }
